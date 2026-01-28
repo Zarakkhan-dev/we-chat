7d361dfd38d982d646750c5a6e28fb40e2d1ba75
Zarak Khan <119649492+Zarakkhan-dev@users.noreply.github.com>
2026-01-28 22:18:35 +0500

    fix: correct route handling for serving frontend in production

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -34,7 +34,7 @@ app.use(errorHandler);
 if(process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../../web/dist")))
 
-  app.get("/{*any}", (req, res) => {
+  app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname,"../../web/dist/index.html"))
   })
 }
